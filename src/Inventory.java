ab8ae9c3c39fa6d3f22a219e459347579d59aabb
sulsul2 <110590472+sulsul2@users.noreply.github.com>
2023-04-19 20:05:40 +0700

    Update Inventory.java

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,12 +1,45 @@
 import java.util.ArrayList;
 
 public class Inventory {
-    private ArrayList <Item> itemList = new ArrayList<>();
+    private ArrayList <Item> itemList;
+    private int inventoryCount;
 
     
-    public Inventory(Item ... items) {
-        for(Item temp : items) {
-            itemList.add(temp);
+    public Inventory() {
+        itemList = new ArrayList<>();
+        inventoryCount = 0;
+    }
+
+    public ArrayList getInventory(){
+        return itemList;
+    }
+
+    public int getInventoryCount(){
+        return inventoryCount;
+    }
+
+    public void addInventory(Item items){
+        itemList.add(items);
+    }
+
+    public void deleteInventory(Item items){
+        int idx = 0;
+        int idxhsl = -1;
+        for (Item i : itemList){
+            if(i.name.equals(items.name)){
+                idxhsl = idx;
+            }
+            else {
+                idx++;
+            }
+        }
+        if (idxhsl != -1){
+            itemList.remove(idxhsl);
+        }
+    }
+    public void printInventory(){
+        for (Item i :itemList){
+            System.out.prinln(i.name);
         }
     }
 }
