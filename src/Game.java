5dc2fa9a5776a0112410c73502362466e46668f5
Fakhri Putra Mahardika <fakhriputramahardika@gmail.com>
2023-04-22 19:25:15 +0700

    feat : menu and object game

---
+++ src/Game.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Game {
+    private static World world = World.getInstance();
+    private static Scanner input = new Scanner(System.in);
+    private static String command;
+    private static Boolean playing = true;
+    public static void start() {
+        String command = CLI.opening();
+        while (!(command.equals("startgame") || command.equals("1"))) {
+            command = CLI.wrongInput();
+        }
+        while(playing) {
+            command = CLI.ListMenu();
+            switch(command.toLowerCase()) {
+                case "1","startgame" :
+                    CLI.repeatedStart();
+                    break;
+                case "2","help" :
+                    Menu.help();
+                    break;
+                case "3","exit" :
+                    playing = false;
+                    Menu.exit();
+                    break;
+                default :
+            }
+        }
+
+    }
+}
