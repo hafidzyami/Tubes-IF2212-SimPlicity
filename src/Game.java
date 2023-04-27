23a4770423f083b71a1d418d7653527886543a94
Fakhri Putra Mahardika <fakhriputramahardika@gmail.com>
2023-04-27 18:51:11 +0700

    fix : menu and main

--- src/Game.java
+++ src/Game.java
@@ -16,24 +16,45 @@ public class Game {
                 case "1","startgame" :
                     CLI.repeatedStart();
                     break;
+
                 case "2","help" :
                     Menu.help();
                     break;
+
                 case "3","exit" :
                     playing = false;
                     Menu.exit();
                     break;
+
                 case "4","view sim info" :
                     Menu.viewSimInfo(world);
                     break;
+
                 case "5", "view current location":
                     Menu.viewCurrentLocation(world.getPlayedSim());
+                    break;
+
+                case "6", "view inventory":
+                    Menu.viewInventory();;
+                    break;
+
+                case "7", "upgrade house":
+                    Menu.upgradeHouse();
+                    break;
+
+                case "8", "move room":
+                    Menu.moveRoom();;
+                    break;
+
+                case "9", "edit room":
+                    Menu.editRoom();;
                     break;
                 case "10", "add sim":
                     System.out.println("Masukkan nama Sim");
                     String name = input.nextLine();
                     Menu.addSim(world, name);
                     break;
+
                 case "11", "change sim":
                     if(world.getSimList().size() > 0){
                         System.out.println("Daftar Sim yang tersedia :");
@@ -54,6 +75,22 @@ public class Game {
                     else{
                         System.out.println("Tidak ada Sim untuk dipilih! Silahkan add Sim!");
                     }
+                    break;
+
+                case "12","list object" :
+                    Menu.listObject(world);
+                    break;
+
+                case "13","go to object" :
+                    Menu.goToObject(world, "barang");
+                    break;
+
+                case "14","action" :
+                    Menu.action();
+                    break;
+
+                case "15","change job" :
+                    Menu.changeJob("Badut");
                     break;
                 default :
             }
