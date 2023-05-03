e6287fb6cf6d9ae5c619e8718a66f488e8cf85ba
Fakhri Putra Mahardika <fakhriputramahardika@gmail.com>
2023-05-03 19:05:26 +0700

    fix : worldclock tidak tidur tidak berak

--- src/WorldClock.java
+++ src/WorldClock.java
@@ -1,13 +1,38 @@
+import java.io.NotSerializableException;
+
 public class WorldClock  {
     private int gameDay;
     private int totalElapsed; //satuan detik
+    private boolean notSleep;
+    private int lastSleepTime;
+    private int sleepTimer;
+    private boolean notPoop;
+    private int poopTimer;
 
     public WorldClock() {
         gameDay = 1;
+        notSleep = true;
+        notPoop = false;
     }
 
     public void updateTime(int duration) {
         this.totalElapsed += duration;
+        if (notSleep) {
+            this.sleepTimer += duration;
+        }
+
+        if (notPoop) {
+            this.poopTimer += duration;
+        }
+    }
+
+    //getter
+    public boolean getNotSleep() {
+        return notSleep;
+    }
+
+    public boolean getNotPoop() {
+        return notPoop;
     }
 
     public int getDay() {
@@ -26,6 +51,15 @@ public class WorldClock  {
         return gameDay * 720 - totalElapsed;
     }
 
+    //Setter
+    public void setNotPoop(boolean status) {
+        this.notPoop = status;
+    }
+
+    public void setNotSleep(boolean status) {
+        this.notSleep = status;
+    }
+
     // Delay dengan duration dalam detik
     public void wait(int duration) {
         try {
@@ -35,6 +69,7 @@ public class WorldClock  {
             Thread.interrupted();
         }
     }
+
     public static void main(String[] args) {
         WorldClock jam = new WorldClock();
         jam.updateTime(40);
